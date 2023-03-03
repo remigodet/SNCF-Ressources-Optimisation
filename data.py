14b5a70a0e54e6642dd16414557b014b94fa5042
remi godet <remgodet@outlook.fr>
2023-03-03 12:11:56 +0100

    added occupation v1.0

--- data.py
+++ data.py
@@ -1,12 +1,24 @@
 import pandas as pd
 from pandas import read_excel
+from tqdm import tqdm
 NAME = "instance_WPY_realiste.xls"
 data = {}
+p_bar = tqdm(range(5), desc="LOADING DATA")
 data["taches_df"] = pd.read_excel(f"./Data SNCF/{NAME}", sheet_name="Taches humaines")
+p_bar.update(1)
+p_bar.refresh()
 data["chantiers_df"] = pd.read_excel(f"./Data SNCF/{NAME}", sheet_name="Chantiers")
+p_bar.update(1)
+p_bar.refresh()
 data["machines_df"] = pd.read_excel(f"./Data SNCF/{NAME}", sheet_name="Machines")
+p_bar.update(1)
+p_bar.refresh()
 data["sillons_df"] = pd.read_excel(f"./Data SNCF/{NAME}", sheet_name="Sillons")
+p_bar.update(1)
+p_bar.refresh()
 data["correspondances_df"] = pd.read_excel(f"./Data SNCF/{NAME}", sheet_name="Correspondances")
+p_bar.update(1)
+p_bar.refresh()
 
 def get_all_pandas():
     return data
