4eb3d920fab61c250edb5b037fd2da42bb7e9e7e
Titouan Lévêque <titouan.leveque@student-cs.fr>
2023-03-04 00:22:07 +0100

    indisponibilité fonctionnelle?

--- contraintes.py
+++ contraintes.py
@@ -49,41 +49,112 @@ def generate_contraintes(m, dataframes, var_dict):
 
 ##### Indisponibilités #####
 
-indisp_dico = {}
-for machine in list(machines_df["Machine"]):
-    strTot = machines_df[machines_df["Machine"]
-                         == machine]["Indisponibilites"].iloc[0]
-    if strTot != "0":
-        strBis = strTot.split(";")
-        strTer = []
-        for str in strBis:
-            split = str[1:-1].split(",")
-            [str1, str2] = split
-            strTer.append((str1, str2))
-        strQuad = []
-        for (strA, strB) in strTer:
-            [str3, str4] = strB.split("-")
-            strQuad.append((strA, str3, str4))
+    mach_indisp_dico = {}
+    for machine in list(machines_df["Machine"]):
+        strTot = machines_df[machines_df["Machine"]
+                             == machine]["Indisponibilites"].iloc[0]
+        if strTot != 0:
+            strBis = strTot.split(";")
+            strTer = []
+            for str in strBis:
+                split = str[1:-1].split(",")
+                [str1, str2] = split
+                strTer.append((str1, str2))
+            strQuad = []
+            for (strA, strB) in strTer:
+                [str3, str4] = strB.split("-")
+                strQuad.append((strA, str3, str4))
 
-        strQuint = []
-        for (strA, strC, strD) in strQuad:
-            [strC1, strC2] = strC.split(":")
-            strC3 = int(strC1)*60 + int(strC2)
-            [strD1, strD2] = strD.split(":")
-            strD3 = int(strD1)*60 + int(strD2)
-            strQuint.append((int(strA), strC3, strD3))
+            strQuint = []
+            for (strA, strC, strD) in strQuad:
+                [strC1, strC2] = strC.split(":")
+                strC3 = int(strC1)*60 + int(strC2)
+                [strD1, strD2] = strD.split(":")
+                strD3 = int(strD1)*60 + int(strD2)
+                strQuint.append((int(strA), strC3, strD3))
 
-        indispList = []
-        for (a, b, c) in strQuint:
-            debut = (a-1)*60*24 + b
-            if c <= b:
-                fin = a*60*24 + c
-            else:
-                fin = (a-1)*60*24 + c
+            indispList = []
+            for (a, b, c) in strQuint:
+                debut = (a-1)*60*24 + b
+                if c <= b:
+                    fin = a*60*24 + c
+                else:
+                    fin = (a-1)*60*24 + c
 
-            indispList.append((debut, fin))
+                indispList.append((debut, fin))
 
-        indisp_dico[machine] = indispList
+            mach_indisp_dico[machine] = indispList
+
+    chan_indisp_dico = {}
+    for chantier in list(chantiers_df["Chantier"]):
+        strTot = chantiers_df[chantiers_df["Chantier"]
+                              == chantier]["Indisponibilites"].iloc[0]
+        if strTot != 0:
+            strBis = strTot.split(";")
+            strTer = []
+            for str in strBis:
+                split = str[1:-1].split(",")
+                [str1, str2] = split
+                strTer.append((str1, str2))
+            strQuad = []
+            for (strA, strB) in strTer:
+                [str3, str4] = strB.split("-")
+                strQuad.append((strA, str3, str4))
+
+            strQuint = []
+            for (strA, strC, strD) in strQuad:
+                [strC1, strC2] = strC.split(":")
+                strC3 = int(strC1)*60 + int(strC2)
+                [strD1, strD2] = strD.split(":")
+                strD3 = int(strD1)*60 + int(strD2)
+                strQuint.append((int(strA), strC3, strD3))
+
+            indispList = []
+            for (a, b, c) in strQuint:
+                debut = (a-1)*60*24 + b
+                if c <= b:
+                    fin = a*60*24 + c
+                else:
+                    fin = (a-1)*60*24 + c
+
+                indispList.append((debut, fin))
+
+            chan_indisp_dico[chantier] = indispList
+
+    def no_overlap_indisp(x1, x2, t1, t2, m):
+        A = m.addVar(vtype=GRB.INTEGER, ub=1, lb=0)
+        M = 2000000
+        m.addConstr(M*A+x1-x2-t2 >= 0)
+        m.addConstr(M*(1-A)+x2-x1 - t1 >= 0)
+
+    machine_lenght = {'DEP': 20, 'FOR': 15, 'DEG': 15}
+    tache_chantier = {{'WPY_DEP': ['arrivée Reception', 'préparation tri', 'débranchement'],
+                       'WPY_FOR': ['appui voie + mise en place câle', 'attelage véhicules'],
+                       'WPY_DEG': ['dégarage / bouger de rame', 'essai de frein départ']}}
+    humaine_lenght = {'arrivée Reception': 15, 'préparation tri': 45, 'débranchement': 20,
+                      'appui voie + mise en place câle': 15, 'attelage véhicules': 149,
+                      'dégarage / bouger de rame': 15, 'essai de frein départ': 20}
+
+    for machine in mach_indisp_dico.keys():
+        for indisp_list in mach_indisp_dico[machine]:
+            for indisp in indisp_list:
+                for tache in var_dict[machine]:
+                    x1 = tache
+                    t1 = machine_lenght[machine]
+                    x2 = indisp[0]
+                    t2 = indisp[1]-indisp[0]
+                    no_overlap_indisp(x1, x2, t1, t2)
+
+    for chantier in chan_indisp_dico.keys():
+        for indisp_list in mach_indisp_dico[chantier]:
+            for indisp in indisp_list:
+                for type_de_tache in tache_chantier[chantier]:
+                    for tache in var_dict[type_de_tache]:
+                        x1 = tache
+                        t1 = humaine_lenght[chantier]
+                        x2 = indisp[0]
+                        t2 = indisp[1]-indisp[0]
+                        no_overlap_indisp(x1, x2, t1, t2)
 
     # for machine in machines_dico.keys():
     #     for sillon in dico[machine].keys():
@@ -111,6 +182,7 @@ for machine in list(machines_df["Machine"]):
     ##### Wagons tous présent avant assemblage du sillon #####
 
     ##### taches humaines (chaines et debut synchro avec les taches machines) #####
+
 
 for tache in var_dict.keys():
     for sillon in var_dict[tache].keys():
