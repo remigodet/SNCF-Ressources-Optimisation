13c0730a5f36fa78c6b345807f5a891e90258d61
remi godet <remgodet@outlook.fr>
2023-03-15 03:13:05 +0100

    occupation V2

--- utils.py
+++ utils.py
@@ -15,3 +15,17 @@ def min_to_jour(min):
     if len(minute) == 1:
         minute = "0" + minute
     return (jour + "/" + mois, heure + ":" + minute)
+
+def get_min_from_sillonid(HJCHOICE, sillon_id, sillons_df):
+    '''
+    HJCHOICE must be DEP or ARR
+    '''
+    if HJCHOICE not in ["ARR", "DEP"]:
+        raise KeyError(HJCHOICE)
+    jour = sillons_df[sillons_df["train_id"]== sillon_id][f"J{HJCHOICE}"].iloc[0]
+    jour = str(jour)[0:2]
+    heure = sillons_df[sillons_df["train_id"]
+                        == sillon_id][f"H{HJCHOICE}"].iloc[0]
+    [heure, minute] = heure.split(":")
+    h_dep = (int(jour)-8)*60*24 + int(heure)*60 + int(minute)
+    return h_dep
